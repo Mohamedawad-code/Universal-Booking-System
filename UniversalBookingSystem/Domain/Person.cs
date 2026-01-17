e42df6c0443e4e8a0f3f81d2b6fb8528b2e9bb8e
Mohamedawad-code <awadmohamed7000@gmail.com>
2026-01-17 10:51:32 +0100

    Add Person.cs Add the variables, constrtuctor, (not finished)

--- UniversalBookingSystem/Domain/Person.cs
+++ UniversalBookingSystem/Domain/Person.cs
@@ -2,5 +2,93 @@
 {
     public class Person
     {
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Phonenumber { get; private set; }
+
+        public Person(int id, string name, string email, string phonenumber)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException("Missing name");
+
+            if (string.IsNullOrEmpty(email))
+                throw new ArgumentException("Email Missing");
+
+            if (phonenumber.Length < 5)
+                throw new ArgumentException("PhoneNumber must be longer");
+
+            Id = id;
+            Name = name;
+            Email = email;
+            Phonenumber = phonenumber;
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
